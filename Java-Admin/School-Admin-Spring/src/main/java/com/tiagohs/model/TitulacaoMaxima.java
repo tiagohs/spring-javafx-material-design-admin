1cdcd6796df2b9f29ba76596f7df4613f746bf22
tisil <tiago.silva.93@hotmail.com>
2017-07-10 16:40:46 -0300

    excluido.

--- Java-Admin/School-Admin-Spring/src/main/java/com/tiagohs/model/TitulacaoMaxima.java
+++
@@ -1,5 +0,0 @@
-package com.tiagohs.model;
-
-public enum TitulacaoMaxima {
-	GRADUACAO, ESPECIALIZACAO, MESTRADO, DOUTORADO;
-}
--- Java-Admin/School-Admin-Spring/src/main/java/com/tiagohs/service/UsuarioService.java
+++
@@ -1,9 +0,0 @@
-package com.tiagohs.service;
-
-import com.tiagohs.model.Usuario;
-
-public interface UsuarioService extends IBaseService<Usuario> {
-	
-	public Usuario findUserByEmail(String email);
-	public void saveUser(Usuario usuario);
-}
