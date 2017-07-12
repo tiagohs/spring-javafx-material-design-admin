0aec359d4779839d7a44f3029df92342f36cce4c
tisil <tiago.silva.93@hotmail.com>
2017-07-12 18:30:08 -0300

    Mais erros vistos.

--- src/main/java/com/tiagohs/controller/AlunoController.java
+++ src/main/java/com/tiagohs/controller/AlunoController.java
@@ -2,7 +2,14 @@ package com.tiagohs.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
